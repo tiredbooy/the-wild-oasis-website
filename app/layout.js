8b43e2520e550dd9f828ffab70af8475e9935322
tiredbooy <mahdykazemyo1i2@gmail.com>
2025-09-20 18:07:16 +0330

    added font and configured metadata

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,31 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 
-// INSTALL ICON LIBRARY (( @heroicons/react ))
+import { Josefin_Sans } from "next/font/google";
 
-import "@/app/_styles/globals.css"
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+import "@/app/_styles/globals.css";
 
 export const metadata = {
-  title: "The Wild Oasis",
+  // title: "The Wild Oasis",
+  title: {
+    template: "%s - The Wild Oasis",
+    default: "Welcome - The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, Located in the heart of Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-primary-950 text-primary-100">
+      <body
+        className={`${josefin.className} min-h-screen bg-primary-950 text-primary-100`}
+      >
         <header>
           <Navigation />
           <Logo />
