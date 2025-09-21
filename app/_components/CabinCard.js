71bce2f8d65d347ebafa1f47deee8895e66c5038
tiredbooy <mahdykazemyo1i2@gmail.com>
2025-09-21 11:51:34 +0330

    load cabins data from database

--- app/_components/CabinCard.js
+++ app/_components/CabinCard.js
@@ -1,36 +1,41 @@
+import Image from "next/image";
 import { UsersIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 
 function CabinCard({ cabin }) {
   const { id, name, maxCapacity, regularPrice, discount, image } = cabin;
 
   return (
-    <div className="flex border-primary-800 border">
-      <img
-        src={image}
-        alt={`Cabin ${name}`}
-        className="flex-1 border-r border-primary-800"
-      />
+    <div className="flex border border-primary-800">
+      <div className="relative flex-1">
+        <Image
+          src={image}
+          fill
+          alt={`Cabin ${name}`}
+          className="object-cover border-r border-primary-800"
+        />
+      </div>
 
       <div className="flex-grow">
         <div className="pt-5 pb-4 px-7 bg-primary-950">
-          <h3 className="text-accent-500 font-semibold text-2xl mb-3">
+          <h3 className="mb-3 text-2xl font-semibold text-accent-500">
             Cabin {name}
           </h3>
 
-          <div className="flex gap-3 items-center mb-2">
-            <UsersIcon className="h-5 w-5 text-primary-600" />
+          <div className="flex items-center gap-3 mb-2">
+            <UsersIcon className="w-5 h-5 text-primary-600" />
             <p className="text-lg text-primary-200">
               For up to <span className="font-bold">{maxCapacity}</span> guests
             </p>
           </div>
 
-          <p className="flex gap-3 justify-end items-baseline">
+          <p className="flex items-baseline justify-end gap-3">
             {discount > 0 ? (
               <>
                 <span className="text-3xl font-[350]">
                   ${regularPrice - discount}
                 </span>
-                <span className="line-through font-semibold text-primary-600">
+                <span className="font-semibold line-through text-primary-600">
                   ${regularPrice}
                 </span>
               </>
@@ -41,13 +46,13 @@ function CabinCard({ cabin }) {
           </p>
         </div>
 
-        <div className="bg-primary-950 border-t border-t-primary-800 text-right">
-          <a
+        <div className="text-right border-t bg-primary-950 border-t-primary-800">
+          <Link
             href={`/cabins/${id}`}
-            className="border-l border-primary-800 py-4 px-6 inline-block hover:bg-accent-600 transition-all hover:text-primary-900"
+            className="inline-block px-6 py-4 transition-all border-l border-primary-800 hover:bg-accent-600 hover:text-primary-900"
           >
             Details & reservation &rarr;
-          </a>
+          </Link>
         </div>
       </div>
     </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images : {
+        remotePatterns : [
+            {
+                protocol : "https",
+                hostname : "onscssytzhjeksslojho.supabase.co",
+                port : "",
+                pathname : "/storage/v1/object/public/cabin-images/**"
+            }
+        ]
+    }
+};
 
 export default nextConfig;
