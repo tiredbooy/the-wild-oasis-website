251127979ef41fac6e9bf15ccf703dcbc67d2481
tiredbooy <mahdykazemyo1i2@gmail.com>
2025-09-23 19:53:41 +0330

    render server component in a client component

--- app/_components/TextExpander.js
+++ app/_components/TextExpander.js
@@ -1,4 +1,6 @@
+"use client"
 import { useState } from 'react';
+import Logo from './Logo';
 
 function TextExpander({ children }) {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -10,7 +12,7 @@ function TextExpander({ children }) {
     <span>
       {displayText}{' '}
       <button
-        className='text-primary-700 border-b border-primary-700 leading-3 pb-1'
+        className='pb-1 leading-3 border-b text-primary-700 border-primary-700'
         onClick={() => setIsExpanded(!isExpanded)}
       >
         {isExpanded ? 'Show less' : 'Show more'}
