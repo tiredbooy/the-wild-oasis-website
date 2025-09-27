11ba731f5059dbaa7dbf854547fc9c5b18f6d008
tiredbooy <mahdykazemyo1i2@gmail.com>
2025-09-27 18:02:16 +0330

    created delete reservation logic

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -1,10 +1,15 @@
-import { TrashIcon } from '@heroicons/react/24/solid';
+"use client";
+import { TrashIcon } from "@heroicons/react/24/solid";
+import { deleteReservation } from "../_lib/actions";
 
 function DeleteReservation({ bookingId }) {
   return (
-    <button className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'>
-      <TrashIcon className='h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors' />
-      <span className='mt-1'>Delete</span>
+    <button
+      onClick={() => deleteReservation(bookingId)}
+      className="flex items-center flex-grow gap-2 px-3 text-xs font-bold uppercase transition-colors group text-primary-300 hover:bg-accent-600 hover:text-primary-900"
+    >
+      <TrashIcon className="w-5 h-5 transition-colors text-primary-600 group-hover:text-primary-800" />
+      <span className="mt-1">Delete</span>
     </button>
   );
 }
