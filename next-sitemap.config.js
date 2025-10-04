c749a90c93f706760a4ee7ca24d189737085d246
tiredbooy <mahdykazemyo1i2@gmail.com>
2025-10-04 23:17:05 +0330

    added sitemap generator

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://the-wild-oasis-website-iota-dusky.vercel.app', 
+  generateRobotsTxt: true,           
+  sitemapSize: 7000,                            
+  changefreq: 'monthly',
+  priority: 0.7,
+};
